b5a6d68d737fc88ad2ac461bc260c378d784848d
rapidcoderx <sathishkr0582@gmaill.com>
2024-01-07 16:54:31 -0700

    Initial commit

--- index.js
+++ index.js
@@ -1,14 +1,95 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const {LRUCache} = require('lru-cache');
 const path = require('path');
-require('dotenv').config();
-
+const amqp = require('amqplib');
 const app = express();
+
 app.use(bodyParser.json());
 
-const initCache = () => {
-    const options = {
+// RabbitMQ's connection details with TLS
+const rabbitMQOptions = {
+    protocol: process.env.RABBITMQ_PROTOCOL,
+    hostname: process.env.RABBITMQ_HOSTNAME,
+    port: process.env.RABBITMQ_PORT,
+    username: process.env.RABBITMQ_USERNAME,
+    password: process.env.RABBITMQ_PASSWORD,
+    locale: process.env.RABBITMQ_LOCALE,
+    frameMax: process.env.RABBITMQ_FRAME_MAX,
+    heartbeat: process.env.RABBITMQ_HEARTBEAT
+};
+
+//Initial connect to RabbitMQ and channel create
+const AMQP_CONNECTION_STRING = `amqps://${rabbitMQOptions.username}:${rabbitMQOptions.password}@${rabbitMQOptions.hostname}:${rabbitMQOptions.port}`;
+
+const exchangeName = process.env.EXCHANGE_NAME || 'core';
+const topicName = process.env.TOPIC_NAME || 'banking';
+const SUCCESS_MESSAGE = 'Message published to RabbitMQ';
+const FAILURE_MESSAGE = 'Failed to publish message to RabbitMQ';
+let channel = null;
+
+const setupRabbitMQ = async () => {
+    try {
+        const connection = await amqp.connect(AMQP_CONNECTION_STRING);
+        const currentChannel = await connection.createChannel();
+
+        await currentChannel.assertExchange(exchangeName, 'topic', {durable: true});
+
+        channel = currentChannel;
+
+        console.log('Connected to RabbitMQ successfully.');
+    } catch (error) {
+        console.error('Failed to connect to RabbitMQ:', error);
+    }
+};
+
+(async () => {
+    await setupRabbitMQ();
+    await consumeFromRabbitMQ();
+    // The rest of your application code here...
+})().catch(error => console.error(`An error occurred while setting up the application: ${error.message}`));
+
+const measureExecutionTime = (func) => {
+    const startTime = Date.now();
+    func();
+    return Date.now() - startTime;
+};
+
+const sendJsonResponse = (res, status, message, error) => {
+    const responseContent = error
+        ? {status, message, error: error.message}
+        : {status, message};
+    const statusCode = status === 'success' ? 200 : 500;
+
+    res.status(statusCode).json(responseContent);
+};
+
+// Express endpoint to receive a message and publish it to RabbitMQ
+const publishToRabbitMQ = (channel, exchangeName, topicName, bufferContent) => {
+    channel.publish(exchangeName, topicName, bufferContent);
+};
+
+const publishMessage = async (req, res) => {
+    const bufferContent = Buffer.from(JSON.stringify(req.body));
+
+    const executeAndMeasureTime = () => {
+        if (channel) {
+            publishToRabbitMQ(channel, exchangeName, topicName, bufferContent);
+        } else {
+            console.error('Failed to publish message to RabbitMQ due to connection error');
+            sendJsonResponse(res, 'failure', FAILURE_MESSAGE, {message: 'Connection error'});
+        }
+    };
+
+    const elapsedTime = measureExecutionTime(executeAndMeasureTime);
+
+    res.set('Processing-Time', `${elapsedTime}ms`);
+    sendJsonResponse(res, 'success', SUCCESS_MESSAGE);
+};
+
+const initLRUCache = () => {
+    const cacheOptions = {
         max: 500,
         ttl: 1000 * 60 * 15,
         allowStale: false,
@@ -16,13 +97,24 @@ const initCache = () => {
         updateAgeOnHas: false,
     };
 
-    return new LRUCache(options);
+    return new LRUCache(cacheOptions);
 };
 
-const cache = initCache();
+const cache = initLRUCache();
 let currentDataKey = 1;
-
+const topicCache = initLRUCache();
 const sortByTimestamp = (values) => Array.isArray(values) ? values.sort((a, b) => b['timestamp'] - a['timestamp']) : [];
+
+const consumeFromRabbitMQ = async () => {
+    // Ensure the topic queue exists
+    let queue = await channel.assertQueue('', {exclusive: true});
+    channel.bindQueue(queue.queue, exchangeName, topicName);
+    channel.consume(queue.queue, (data) => {
+        const message = JSON.parse(data.content);
+        storeDataInCache(topicCache, currentDataKey++, message);
+        channel.ack(data);
+    }, {noAck: false});
+};
 
 const storeDataInCache = (key, data) => {
     cache.set(key, {data: data, timestamp: Date.now()});
@@ -61,19 +153,40 @@ app.post('/api/data', (req, res) => {
     res.status(201).json({message: 'Data stored successfully'});
 });
 
+app.post('/api/topicData', (req, res) => {
+    Object.assign(req.body, {key: currentDataKey++});
+    storeDataInCache(topicCache, req.body.key.toString(), req.body);
+    res.status(201).json({message: 'Data stored successfully'});
+});
+
+app.get('/api/topicData', (req, res) => {
+    let cacheKeys = Array.from(topicCache.keys());
+    let mappedValues = Array.from(topicCache.values()).map((value, index) => mapValuesWithTimestamp(value, cacheKeys[index]));
+    res.json(sortByTimestamp(mappedValues));
+});
+
+app.get('/api/topicView', (req, res) => {
+    let cacheKeys = Array.from(topicCache.keys());
+    let mappedValues = Array.from(topicCache.values()).map((value, index) => mapValuesWithTimestamp(value, cacheKeys[index]));
+    let sortedValues = sortByTimestamp(mappedValues);
+    res.render('data', {values: sortedValues.slice(-15)});
+});
+
+app.post('/publish', publishMessage);
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/api/data', (req, res) => {
-    let keys = Array.from(cache.keys());
-    let values = Array.from(cache.values()).map((value, index) => mapValuesWithTimestamp(value, keys[index]));
-    res.json(sortByTimestamp(values));
+    let cacheKeys = Array.from(cache.keys());
+    let mappedValues = Array.from(cache.values()).map((value, index) => mapValuesWithTimestamp(value, cacheKeys[index]));
+    res.json(sortByTimestamp(mappedValues));
 });
 
 app.get('/api/view', (req, res) => {
-    let keys = Array.from(cache.keys());
-    let values = Array.from(cache.values()).map((value, index) => mapValuesWithTimestamp(value, keys[index]));
-    let sortedValues = sortByTimestamp(values);
+    let cacheKeys = Array.from(cache.keys());
+    let mappedValues = Array.from(cache.values()).map((value, index) => mapValuesWithTimestamp(value, cacheKeys[index]));
+    let sortedValues = sortByTimestamp(mappedValues);
     res.render('data', {values: sortedValues.slice(-15)});
 });
 
